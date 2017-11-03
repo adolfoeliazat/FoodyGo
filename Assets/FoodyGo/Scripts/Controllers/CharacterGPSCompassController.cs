4106c63897769c0d9c08d5ebc408015ec6368049
maniac-tech <ajj_2396@yahoo.com>
2017-11-04 01:02:52 +0530

    Created CharacterCompass control script

---
+++ Assets/FoodyGo/Scripts/Controllers/CharacterGPSCompassController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGPSCompassController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.compass.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Orient an Object to POint to magnetic north and adjust map reversal
+		var heading = 180 + Input.compass.magneticHeading;
+		var rotation = Quaternion.AngleAxis(heading,Vector3.up);
+		transform.rotation=rotation;
+	}
+}
